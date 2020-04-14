75e88657d66504e73f1b784897cf615194added2
sarbor <sarboroy99@gmail.com>
2020-04-14 14:39:07 -0700

    added POST method to users api

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+
 app = Flask(__name__)
 
 users = { 
@@ -36,6 +39,30 @@ users = {
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/users')
-def get_users():
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
    return users
+
+@app.route('/users', methods=['GET', 'POST'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
